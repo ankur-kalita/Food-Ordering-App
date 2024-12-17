e854f84397958f0c101738b94a07945255a59d1a
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-12-17 19:12:52 +0530

    Button commited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import logoImg from '../assets/logo.jpg'
+import Button from './UI/Button'
 
 export default function Header() {
-    return <header id="main-header">
-        <div id='title'>
-            <img src={logoImg} alt='A restaurant'/>
-            <h1>TogetherDevour</h1>
-        </div>
-        <nav>
-            <button>Cart (0)</button>
-        </nav>
-    </header>
+    return (
+        <header id="main-header">
+            <div id='title'>
+                <img src={logoImg} alt='A restaurant'/>
+                <h1>TogetherDevour</h1>
+            </div>
+            <nav>
+                <Button textOnly={true}>Cart (0)</Button>
+            </nav>
+        </header>
+    )
 }
