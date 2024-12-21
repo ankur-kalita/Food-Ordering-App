1b16971692b89877af610981fc2c30b3783003c3
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-12-21 23:48:49 +0530

    custom hook created

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -23,7 +23,7 @@ export default function Cart() {
     }
 
     return (
-        <Modal className="cart" open={userProgressCtx.progress === 'cart'} onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}>
+        <Modal className="cart" open={userProgressCtx.progress === 'cart'} onClose={userProgressCtx.progress === 'cart' ? handleCloseCart :   null}>
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,21 +1,29 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
 
+const requestConfig = {};
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
+    // const [loadedMeals, setLoadedMeals] = useState([]);
+    // useEffect(() => {
+    //     async function fetchMeals() {
+    //         const response = await fetch('http://localhost:3000/meals');
 
-            if(!response) {
-                //
-            }
+    //         if(!response) {
+    //             //
+    //         }
 
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-        fetchMeals();
-    }, []);
+    //         const meals = await response.json();
+    //         setLoadedMeals(meals);
+    //     }
+    //     fetchMeals();
+    // }, []);
+
+    const {data: loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+    if(isLoading) {
+        return <p>Fetching meals...</p>
+    }
         
    
     return (
